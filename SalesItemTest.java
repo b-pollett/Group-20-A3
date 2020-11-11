17f5ba987c93b31722c756657654fcbd917da140
Bernard Pollett <bpollett18@mun.ca>
2020-11-11 16:23:08 -0330

    Added tests for the addComment Method Added tests for the removeComment Method Added tests for the getNumberofComments Method

--- SalesItemTest.java
+++ SalesItemTest.java
@@ -39,6 +39,19 @@ public class SalesItemTest
     }
 
     /**
+     * Test the number of comments when there are 0 as well when there are multiple
+     */
+    @Test
+    public void testGetCommentNumber()
+    {
+        SalesItem salesIte1 = new SalesItem("Brain surgery for Dummies", 21998);
+        assertEquals(0, salesIte1.getNumberOfComments());
+        salesIte1.addComment("James Duckling", "This book is great. I perform brain surgery every week now.", 4);
+        salesIte1.addComment("Joshua Black", "Not worth the money. The font is too small.", 1);
+        assertEquals(2, salesIte1.getNumberOfComments());
+    }
+
+    /**
      * Test that a comment can be added, and that the comment count is correct afterwards.
      */
     @Test
@@ -58,6 +71,17 @@ public class SalesItemTest
         SalesItem salesIte1 = new SalesItem("Java For Complete Idiots, Vol 2", 19900);
         assertEquals(false, salesIte1.addComment("Joshua Black", "Not worth the money. The font is too small.", -5));
     }
+    
+    /**
+     * Test that an author posts multiple comments is rejected.
+     */
+    @Test
+    public void testMultipleComment()
+    {
+        SalesItem salesIte1 = new SalesItem("Java For Complete Idiots, Vol 2", 19900);
+        salesIte1.addComment("Joshua Black", "Not worth the money. The font is too small.", 1);
+        assertEquals(false, salesIte1.addComment("Joshua Black", "Hated it.", 1));
+    }
 
     /**
      * Test that a sales item is correctly initialised (name and price).
@@ -76,6 +100,42 @@ public class SalesItemTest
         SalesItem salesIte1 = new SalesItem("Brain Surgery for Dummies.", 9899);
         assertEquals(true, salesIte1.addComment("Fred", "Great - I perform brain surgery every week now!", 4));
     }
+    
+    /**
+     * Test that a sales item is correctly initialised (name and price).
+     */
+    @Test
+    public void testRemoveComment()
+    {
+        SalesItem salesIte1 = new SalesItem("Java For Complete Idiots, Vol 2", 19900);
+        salesIte1.addComment("Joshua Black", "Not worth the money. The font is too small.", 1);
+        salesIte1.addComment("Fred", "Great - I perform brain surgery every week now!", 4);
+        salesIte1.removeComment(1);
+        assertEquals(1, salesIte1.getNumberOfComments());
+    }
+
+    /**
+     * Test that a sales item is correctly initialised (name and price).
+     */
+    @Test
+    public void testRemoveCommentEmpty()
+    {
+        SalesItem salesIte1 = new SalesItem("Java For Complete Idiots, Vol 2", 19900);
+        salesIte1.removeComment(1);
+        assertEquals(0, salesIte1.getNumberOfComments());
+    }
+    
+    /**
+     * Test that a sales item is correctly initialised (name and price).
+     */
+    @Test
+    public void testRemoveCommentInvalid()
+    {
+        SalesItem salesIte1 = new SalesItem("Java For Complete Idiots, Vol 2", 19900);
+        salesIte1.addComment("Joshua Black", "Not worth the money. The font is too small.", 1);
+        salesIte1.removeComment(2);
+        assertEquals(1, salesIte1.getNumberOfComments());
+    }    
 }
 
 
